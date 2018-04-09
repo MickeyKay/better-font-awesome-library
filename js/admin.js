1e6be07c2f3593976b180c78c74fc0f9fc18d8f3
MickeyKay <mickeyskay@gmail.com>
2018-04-08 18:50:28 -0700

    Clean up whitespace

--- js/admin.js
+++ js/admin.js
@@ -14,42 +14,42 @@
 		return icons.find( function( icon ) {
 			return icon.title == title;
 		});
-    }
+	}
 
 	function icon_shortcode( icon ) {
 		var icon_style_string = icon.style ? ' style="' + icon.style + '"' : '';
 		return '[icon name="' + icon.slug + '"' + icon_style_string + ' class="" unprefixed_class=""]';
 	}
 
-    $( document ).on( 'ready ', function() {
+	$( document ).on( 'ready ', function() {
 
 		$( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) { // Use mousedown even to allow for triggering click later without infinite looping.
 
 			e.preventDefault();
 
-	    	$( this ).not( ' .initialized' )
-	    		.addClass( 'initialized' )
-	    		.iconpicker({
-		    		placement: 'bottomLeft',
-		    		hideOnSelect: true,
-		    		animation: false,
-		    		selectedCustomClass: 'selected',
-		    		icons: icons,
-		    		fullClassFormatter: function( icon_title ) {
-		    			var classes = [];
-		    			var icon = get_icon_by_title( icon_title );
+			$( this ).not( ' .initialized' )
+				.addClass( 'initialized' )
+				.iconpicker({
+					placement: 'bottomLeft',
+					hideOnSelect: true,
+					animation: false,
+					selectedCustomClass: 'selected',
+					icons: icons,
+					fullClassFormatter: function( icon_title ) {
+						var classes = [];
+						var icon = get_icon_by_title( icon_title );
 
-		    			return icon.base_class;
-		    		},
-		    	})
-		    	.find( '.iconpicker-item' ).each( function() {
-		    		var $item = $( this );
-		    		var title = $item.attr( 'title' ).replace( '.', '' );
+						return icon.base_class;
+					},
+				})
+				.find( '.iconpicker-item' ).each( function() {
+					var $item = $( this );
+					var title = $item.attr( 'title' ).replace( '.', '' );
 
-		    		$item.attr( 'title', title );
-		    	});
+					$item.attr( 'title', title );
+				});
 
-		    $( this ).trigger( 'click' );
+			$( this ).trigger( 'click' );
 
 		})
 		.on( 'click', '.bfa-iconpicker', function(e) {
@@ -63,6 +63,6 @@
 			var icon = get_icon_by_title( icon_title );
 			wp.media.editor.insert( icon_shortcode( icon ) );
 		});
-    });
+	});
 
 } )( jQuery );
