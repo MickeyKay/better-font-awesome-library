ca83437bab1f7af249f8f18bb95267673e46df36
Mickey Kay <mickeyskay@gmail.com>
2021-02-07 20:59:58 -0800

    Fix jquery and JS issues (#24)

--- js/admin.js
+++ js/admin.js
@@ -8,61 +8,61 @@
 
 ( function( $ ) {
 
-	var icons = Object.values( bfa_vars.fa_icons );
+  var icons = Object.values( bfa_vars.fa_icons );
 
-	function get_icon_by_title( title ) {
-		return icons.find( function( icon ) {
-			return icon.title == title;
-		});
-	}
+  function get_icon_by_title( title ) {
+    return icons.find( function( icon ) {
+      return icon.title == title;
+    });
+  }
 
-	function icon_shortcode( icon ) {
-		var icon_style_string = icon.style ? ' style="' + icon.style + '"' : '';
-		return '[icon name="' + icon.slug + '"' + icon_style_string + ' class="" unprefixed_class=""]';
-	}
+  function icon_shortcode( icon ) {
+    var icon_style_string = icon.style ? ' style="' + icon.style + '"' : '';
+    return '[icon name="' + icon.slug + '"' + icon_style_string + ' class="" unprefixed_class=""]';
+  }
 
-	$( document ).on( 'ready ', function() {
+  $( function() {
 
-		$( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) { // Use mousedown even to allow for triggering click later without infinite looping.
+    $( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) { // Use mousedown even to allow for triggering click later without infinite looping.
 
-			e.preventDefault();
+      e.preventDefault();
 
-			$( this ).not( ' .initialized' )
-				.addClass( 'initialized' )
-				.iconpicker({
-					placement: 'bottomLeft',
-					hideOnSelect: true,
-					animation: false,
-					selectedCustomClass: 'selected',
-					icons: icons,
-					fullClassFormatter: function( icon_title ) {
-						var classes = [];
-						var icon = get_icon_by_title( icon_title );
+      $( this ).not( ' .initialized' )
+        .addClass( 'initialized' )
+        .iconpicker({
+          placement: 'bottomLeft',
+          hideOnSelect: true,
+          animation: false,
+          selectedCustomClass: 'selected',
+          icons: icons,
+          fullClassFormatter: function( icon_title ) {
+            var classes = [];
+            var icon = get_icon_by_title( icon_title );
 
-						return icon.base_class;
-					},
-				})
-				.find( '.iconpicker-item' ).each( function() {
-					var $item = $( this );
-					var title = $item.attr( 'title' ).replace( '.', '' );
+            return icon.base_class;
+          },
+        })
+        .find( '.iconpicker-item' ).each( function() {
+          var $item = $( this );
+          var title = $item.attr( 'title' ).replace( '.', '' );
 
-					$item.attr( 'title', title );
-				});
+          $item.attr( 'title', title );
+        });
 
-			$( this ).trigger( 'click' );
+      $( this ).trigger( 'click' );
 
-		})
-		.on( 'click', '.bfa-iconpicker', function(e) {
-			e.preventDefault(); // Prevent scrolling to top.
-			$( this ).find( '.iconpicker-search' ).focus();
-		});
+    })
+    .on( 'click', '.bfa-iconpicker', function(e) {
+      e.preventDefault(); // Prevent scrolling to top.
+      $( this ).find( '.iconpicker-search' ).focus();
+    });
 
-		// Set up icon insertion functionality.
-		$( document ).on( 'iconpickerSelect', function( e ) {
-			var icon_title = e.iconpickerItem.context.title.replace( '.', '' );
-			var icon = get_icon_by_title( icon_title );
-			wp.media.editor.insert( icon_shortcode( icon ) );
-		});
-	});
+    // Set up icon insertion functionality.
+    $( '.bfa-iconpicker' ).on( 'iconpickerSelected', function( e ) {
+      var icon_title = e.iconpickerItem.title.replace( '.', '' );
+      var icon = get_icon_by_title( icon_title );
+      wp.media.editor.insert( icon_shortcode( icon ) );
+    });
+  });
 
 } )( jQuery );
