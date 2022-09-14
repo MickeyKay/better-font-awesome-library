cee0797813d5f194cebab269168910e94acb0a78
MickeyKay <mickeyskay@gmail.com>
2022-09-13 22:58:31 -0700

    Clean up whitespace

--- js/admin.js
+++ js/admin.js
@@ -23,56 +23,56 @@
 
 	$( function() {
 
-        // We initialize on click instead of document.ready to ensure
-        // that BFA triggers still work when dynamically loaded later
-        // (e.g. repeatable fields/editors).
-        //
-        // We use the 'mousedown' handler so that we can effectively
-        // initialize the trigger, then manually trigger a 'click'
-        // event to trigger the iconpicker's click handler, all
-        // without causing an infinite loop.
-        $( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) {
+		// We initialize on click instead of document.ready to ensure
+		// that BFA triggers still work when dynamically loaded later
+		// (e.g. repeatable fields/editors).
+		//
+		// We use the 'mousedown' handler so that we can effectively
+		// initialize the trigger, then manually trigger a 'click'
+		// event to trigger the iconpicker's click handler, all
+		// without causing an infinite loop.
+		$( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) {
 
-            var $iconPicker = $( this );
+			var $iconPicker = $( this );
 
-            // Initialize if not already initialized.
-            $iconPicker.not( '.initialized' )
-                .addClass( 'initialized' )
-        		.iconpicker({
-        			placement: 'bottomLeft',
-        			hideOnSelect: true,
-        			icons: icons,
-        			fullClassFormatter: function( icon_title ) {
-        				var classes = [];
-        				var icon = get_icon_by_title( icon_title );
+			// Initialize if not already initialized.
+			$iconPicker.not( '.initialized' )
+				.addClass( 'initialized' )
+				.iconpicker({
+					placement: 'bottomLeft',
+					hideOnSelect: true,
+					icons: icons,
+					fullClassFormatter: function( icon_title ) {
+						var classes = [];
+						var icon = get_icon_by_title( icon_title );
 
-        				return icon.base_class;
-        			},
-        		})
+						return icon.base_class;
+					},
+				})
 
-                // Place cursor focus on the search input.
-                .on( 'iconpickerShown', function( e ) {
-                    $iconPicker.find( '.iconpicker-search' ).trigger( 'focus');
-                })
+				// Place cursor focus on the search input.
+				.on( 'iconpickerShown', function( e ) {
+					$iconPicker.find( '.iconpicker-search' ).trigger( 'focus');
+				})
 
-                // Handle inserting selected icon into editor.
-                .on( 'iconpickerSelected', function( e ) {
-                    var icon_title = e.iconpickerItem.title.replace( '.', '' );
-                    var icon = get_icon_by_title( icon_title );
-                    wp.media.editor.insert( icon_shortcode( icon ) );
-                })
+				// Handle inserting selected icon into editor.
+				.on( 'iconpickerSelected', function( e ) {
+					var icon_title = e.iconpickerItem.title.replace( '.', '' );
+					var icon = get_icon_by_title( icon_title );
+					wp.media.editor.insert( icon_shortcode( icon ) );
+				})
 
-                // Fake a click to ensure trigger the iconpicker's native click handler.
-                .trigger( 'click' )
+				// Fake a click to ensure trigger the iconpicker's native click handler.
+				.trigger( 'click' )
 
-                // Clean up human-readable icon names.
-        		.find( '.iconpicker-item' ).each( function() {
-        			var $item = $( this );
-        			var title = $item.attr( 'title' ).replace( '.', '' );
+				// Clean up human-readable icon names.
+				.find( '.iconpicker-item' ).each( function() {
+					var $item = $( this );
+					var title = $item.attr( 'title' ).replace( '.', '' );
 
-        			$item.attr( 'title', title );
-        		});
-        });
+					$item.attr( 'title', title );
+				});
+		});
 	});
 
 } )( jQuery );
