44a4a87514568164caa18a3f64d07de6671da416
MickeyKay <mickeyskay@gmail.com>
2022-09-13 22:56:19 -0700

    Actually repair dynamic trigger logic

--- js/admin.js
+++ js/admin.js
@@ -26,7 +26,12 @@
         // We initialize on click instead of document.ready to ensure
         // that BFA triggers still work when dynamically loaded later
         // (e.g. repeatable fields/editors).
-        $( 'body' ).on( 'click', '.bfa-iconpicker', function(e) {
+        //
+        // We use the 'mousedown' handler so that we can effectively
+        // initialize the trigger, then manually trigger a 'click'
+        // event to trigger the iconpicker's click handler, all
+        // without causing an infinite loop.
+        $( 'body' ).on( 'mousedown', '.bfa-iconpicker', function(e) {
 
             var $iconPicker = $( this );
 
@@ -45,24 +50,27 @@
         			},
         		})
 
+                // Place cursor focus on the search input.
+                .on( 'iconpickerShown', function( e ) {
+                    $iconPicker.find( '.iconpicker-search' ).trigger( 'focus');
+                })
+
+                // Handle inserting selected icon into editor.
+                .on( 'iconpickerSelected', function( e ) {
+                    var icon_title = e.iconpickerItem.title.replace( '.', '' );
+                    var icon = get_icon_by_title( icon_title );
+                    wp.media.editor.insert( icon_shortcode( icon ) );
+                })
+
+                // Fake a click to ensure trigger the iconpicker's native click handler.
+                .trigger( 'click' )
+
                 // Clean up human-readable icon names.
         		.find( '.iconpicker-item' ).each( function() {
         			var $item = $( this );
         			var title = $item.attr( 'title' ).replace( '.', '' );
 
         			$item.attr( 'title', title );
-        		});
-
-                // Place cursor focus on the search input.
-                .on( 'iconpickerShown', function( e ) {
-        			$iconPicker.find( '.iconpicker-search' ).trigger( 'focus');
-        		});
-
-        		// Handle inserting selected icon into editor.
-        		.on( 'iconpickerSelected', function( e ) {
-        			var icon_title = e.iconpickerItem.title.replace( '.', '' );
-        			var icon = get_icon_by_title( icon_title );
-        			wp.media.editor.insert( icon_shortcode( icon ) );
         		});
         });
 	});
